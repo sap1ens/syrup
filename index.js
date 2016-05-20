d03676e5b7f9c1b58bdd513909f0e6efcd43d2de
sap1ens <sapiensy@gmail.com>
2016-05-20 14:40:41 -0700

    Little tweak

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-import sprints from './features/sprints';
+import sprints from './features/list-sprints';
 
 sprints();
+
+// TODO:
+// - use minimist for parsing args
+// - pass different args for creating new sprint and listing existing, like npm run test -- --grep="pattern"
