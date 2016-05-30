d41ad107629f7645a7e7b9491db019beefa39c4e
sap1ens <sapiensy@gmail.com>
2016-05-29 21:03:35 -0700

    Create new close-issue feature and use yargs-parser

---
+++ features/clone-issue.js
@@ -0,0 +1,7 @@
+import _ from 'underscore';
+import github from '../github';
+import {user} from '../config';
+
+export default function cloneIssue(repo, issueId) {
+    console.log(`Cloning issue in ${repo} repo with ID ${issueId}`);
+}
